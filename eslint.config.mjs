aaef07a130cc856a0430bc7d26ff7972f0882983
KorbenMakar <korbenmakar@gmail.com>
2024-12-26 18:32:24 +0200

    Meal blog has been implemented:
    *Delta:*
    * EsLint has been updated
    * Page for dynamic meals has been updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,14 @@
-const eslintConfig = {
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "eslint:recommended",
-  ],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "react/jsx-props-no-spreading": "off",
-    "react/react-in-jsx-scope": "off",
-  },
-  settings: {
-    react: {
-      version: "detect",
+import compat from "eslint-config-next";
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
   },
-};
+];
 
 export default eslintConfig;
